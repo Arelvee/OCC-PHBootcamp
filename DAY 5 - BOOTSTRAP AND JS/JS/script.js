c81a8a29291aa2cf7743267ff0a19d7453099f10
Ralph Laurence Visaya <arelvisaya@gmail.com>
2024-03-01 21:08:24 +0800

    Lesson about Bootstrap and Javascript

---
+++ DAY 5 - BOOTSTRAP AND JS/JS/script.js
@@ -0,0 +1,55 @@
+const my_name = "Ralph";
+let my_age = 31;
+
+console.log(my_name);
+console.log(my_age);
+
+
+
+let my_num1 = 20;
+let my_num2 = 90.8;
+
+console.log("This is a number: ", my_num1, my_num2);
+
+
+
+let first_name = "Ralph Laurence";
+let last_name = "Visaya";
+
+console.log("My name is", first_name, last_name);
+
+let isStudent = false;
+
+console.log("This is student is: ", isStudent);
+
+
+let undefinedVar;
+console.log(undefinedVar);
+
+const myNull = null;
+
+console.log(myNull);
+
+//Object
+
+let person = {
+    name: "Ralph",
+    age: 20,
+    city: "Tanauan City"
+
+
+}
+
+console.log(person);
+
+
+let numbers= [1,2,3,4,5];
+console.log(numbers);
+console.log(numbers[0], numbers[4]);
+
+
+function greet(name) {
+    console.log('Hi ' + name + '! I am a function');
+}
+
+greet('Jose');
